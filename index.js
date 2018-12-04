881219f2422207beaaa335b906e5e3dcd417a73e
Marko Bajic <markobajic@Markos-MacBook-Pro.local>
2018-12-04 14:12:23 -0500

    changed init pattern

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+let web3;
 
 module.exports = (endpoint) => {
-  const web3 = require('./lib/eth')(endpoint, protocol = 'http');
+  if (web3 === undefined) {
+    if (endpoint === undefined) {
+      throw new Error('Error! Expected endpoint URL for ethereum network');
+    }
+    
+    web3 = require('./lib/eth')(endpoint, protocol = 'http');
+  }
+  
   const accounts = require('./lib/accounts');
   const contracts = require('./lib/contracts/Contract');
 
-  module.exports = { web3, ...accounts, ...contracts };
+  return { web3, ...accounts, ...contracts };
 };
