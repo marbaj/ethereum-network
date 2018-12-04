18ee517ab53f9d4d125e1a1b19badc6dca135c5b
Marko Bajic <markobajic@Markos-MacBook-Pro.local>
2018-12-04 13:24:35 -0500

    fixed init step

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-let web3, accounts, contracts;
 
-const init = (endpoint) => {
-  web3 = require('./lib/eth')(endpoint, protocol = 'http');
-  accounts = require('./lib/accounts');
-  contracts = require('./lib/contracts/Contract');
+module.exports = (endpoint) => {
+  const web3 = require('./lib/eth')(endpoint, protocol = 'http');
+  const accounts = require('./lib/accounts');
+  const contracts = require('./lib/contracts/Contract');
+
+  module.exports = { web3, ...accounts, ...contracts };
 };
-
-module.exports = { init, web3, ...accounts, ...contracts };
