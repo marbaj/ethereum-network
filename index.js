4e4547c385c98f494de6eb94395e70d412b617c5
Marko Bajic <markobajic@Markos-MacBook-Pro.local>
2018-12-10 13:53:28 -0500

    fix for ws protocol

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let web3;
 
-module.exports = (endpoint) => {
+module.exports = (endpoint, protocol) => {
   if (web3 === undefined) {
     if (endpoint === undefined) {
       throw new Error('Error! Expected endpoint URL for ethereum network');
     }
-    
-    web3 = require('./lib/eth')(endpoint, protocol = 'http');
+
+    web3 = require('./lib/eth')(endpoint, protocol);
   }
   
   const accounts = require('./lib/accounts');
